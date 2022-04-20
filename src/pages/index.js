d096b5eea4a3b812851902354dcd0fe05dcf258a
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-04-20 16:23:45 -0300

    Change Bootstrap library

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,11 @@
 import styles from '../styles/Index.module.css';
+import { Button } from 'react-bootstrap';
 
 export default function Index() {
   return (
     <div className={styles.container}>
-      <button type="button" class="btn btn-primary">Primary</button>
-      <button type="button" class="btn btn-secondary">Secondary</button>
-      <button type="button" class="btn btn-success">Success</button>
-      <button type="button" class="btn btn-danger">Danger</button>
-      <button type="button" class="btn btn-warning">Warning</button>
-      <button type="button" class="btn btn-info">Info</button>
-      <button type="button" class="btn btn-light">Light</button>
-      <button type="button" class="btn btn-dark">Dark</button>
+      <Button variant="primary">Primary</Button>
+      <Button variant="secondary">Secondary</Button>
     </div>
   )
 }
