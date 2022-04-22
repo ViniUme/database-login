378d81b0ca4862e3f9a0abeba35da24c898bdaa2
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-04-22 14:49:57 -0300

    Creating login buttons

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,16 @@
 import styles from '../styles/Index.module.scss';
+import Link from 'next/link';
 
 export default function Index() {
   return (
     <div className={styles.container}>
       <input type="text" placeholder="email" />
       <input type="text" placeholder="password" />
+
+      <section className={styles.section_buttons}>
+        <Link href="#"><a className={styles.login}>Log In</a></Link>
+        <Link href="#"><a className={styles.register}>Register</a></Link>
+      </section>
     </div>
   )
 }
