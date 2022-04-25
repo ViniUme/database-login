a5be3a41cb7e0bcb7d2ff54a32a96fe153d77ad7
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-04-25 15:24:35 -0300

    New Feature: check data's input

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -4,25 +4,34 @@ import styles from '../styles/Register.module.scss';
 export default function Register(){
 
     const [name, setName] = useState("");
-    const [emial, setEmail] = useState("");
+    const [email, setEmail] = useState("");
     const [age, setAge] = useState("");
     const [phone, setPhone] = useState("");
     const [password, setPassword] = useState("");
     const [conPassword, setConPassword] = useState("");
+
+    const PostUser = () => {
+        var check = false;
+        if((name,email,age,phone,password,conPassword != "") && (password == conPassword)){
+            check = true;
+        }
+        
+        if(check == true){console.log('cadastrado')}
+    }
 
     return(
         <div className={styles.container}>
 
             <form className={styles.form}>
                 <input type="text" autoComplete='none' placeholder='name' className={styles.name} value={name} onChange={ e => setName(e.target.value)} />
-                <input type="text" autoComplete='none' placeholder='email' className={styles.email} value={emial} onChange={ e => setEmail(e.target.value)} />
+                <input type="text" autoComplete='none' placeholder='email' className={styles.email} value={email} onChange={ e => setEmail(e.target.value)} />
                 <input type="text" autoComplete='none' placeholder='age' className={styles.age} value={age} onChange={ e => setAge(e.target.value)} />
                 <input type="text" autoComplete='none' placeholder='phone' className={styles.phone} value={phone} onChange={ e => setPhone(e.target.value)} />
                 <input type="text" autoComplete='none' placeholder='password' className={styles.password} value={password} onChange={ e => setPassword(e.target.value)} />
                 <input type="text" autoComplete='none' placeholder='confirm password' className={styles.confirm_password} value={conPassword} onChange={ e => setConPassword(e.target.value)} />
             </form>
 
-            <button className={styles.button}>Register</button>
+            <button className={styles.button} onClick={() => PostUser()}>Register</button>
 
         </div>
     );
