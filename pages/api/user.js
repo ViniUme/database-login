8554497c5101e2e533e1ec6cd71b4781330c876d
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-04-29 17:06:41 -0300

    Fix sign on error

--- pages/api/user.js
+++ pages/api/user.js
@@ -4,11 +4,13 @@ import Connect from "../../utils/database"
 export default async (req,res) => {
     if(req.method == "POST"){
         const {db} = await Connect();
-        const body = await req.body
+        const body = req.body
 
         const find = await db.findOne({email: body.email})
 
-        if(find.email == body.email){
+        console.log(find);
+
+        if(find != null){
             res.status(400).json({message: "user already exist"})
         }
         else{
